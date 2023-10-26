cbf22dd3509fbcce4f0ef4ab40db4ff314705974
Adam Baranczyk <baranczykadam@gmail.com>
2023-10-26 17:10:45 +0200

    added ship

--- src/main/java/org/baranczyk/AsteroidsApp.java
+++ src/main/java/org/baranczyk/AsteroidsApp.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 /**
@@ -17,9 +18,10 @@ public class AsteroidsApp extends Application {
     public void start(Stage stage) {
         var pane = new Pane();
         pane.setPrefSize(600,400);
-
-
-
+        Polygon ship = new Polygon(-5, -5, 10, 0, -5, 5);
+        pane.getChildren().add(ship);
+        ship.setTranslateX(300);
+        ship.setTranslateY(200);
         var scene = new Scene(pane);
         stage.setTitle("Asteroids");
         stage.setScene(scene);
